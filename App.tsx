b7f31232a67f92d43e68e3eedd23e5856dcf5f7b
Edufgs <eduardo.goncal.silva@gmail.com>
2022-10-27 19:34:41 -0400

    Criação da aplicação mobile

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * View = Uma caixa para colocar coisas, igual 'span'
+ * StatusBar = é o componente da barra de status que fica na parte de cima do celular
+ */
+import { View, StatusBar } from 'react-native';
+import SignIn from './src/pages/SignIn';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>      
+      {/* Configura a barra superior do celular, barra de status */}
+      <StatusBar 
+        backgroundColor="#1d1d2e" //Fundo escuro
+        barStyle="light-content" //Cor dos itens da barra de status
+        translucent={false} //Opção dos itens passar por baixo do status bar
+      />
+      <SignIn />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
