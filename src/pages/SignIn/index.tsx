1a4eb031c31881ff8203fe24218f909542fb2427
Edufgs <eduardo.goncal.silva@gmail.com>
2022-11-18 18:05:06 -0400

    Inicio da tela de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,88 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { 
+  View, 
+  Text, 
+  StyleSheet, //é para estilizar as paginas.
+  Image, //é para adicionar imagens
+  TextInput, //entrada de texto
+  TouchableOpacity //Botao de toque
+} from "react-native";
 
 export default function SignIn(){
   return(
-    <View>
-      <Text>Edu Pizza</Text>
+    //Adiciona um grupo de estilo no componente
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        /**
+         * Diz onde está a imagem
+         * require = o o caminho ate a imagem
+         */
+        source={require('../../assets/logo.png')}
+        
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Digite seu email"
+          placeholderTextColor="#F0F0F0" //Cor do placeholder
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Digite sua senha"
+          placeholderTextColor="#F0F0F0"
+          secureTextEntry={true} //Deixa mascarado a senha 
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
+
+//Configuração de estilos
+const styles = StyleSheet.create({
+  //Grupo de estilos
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems: 'center',
+    backgroundColor: '#1d1d2e'
+  },
+  logo:{
+    width: 400,
+    height: 100,
+  },
+  inputContainer:{
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+    paddingHorizontal: 14
+  },
+  input:{
+    width: '95%',
+    height: 40,
+    backgroundColor: '#101026',
+    marginBottom: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#FFF'
+  },
+  button:{
+    width: '95%',
+    height: 40,
+    backgroundColor:'#3fffa3',
+    borderRadius: 4,
+    justifyContent:'center',
+    alignItems: 'center',
+  },
+  buttonText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#101026'
+  }
+})
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { View } from 'react-native'
+//ActivityIndicator é o spinner que fica rodando
+import { View, ActivityIndicator } from 'react-native'
 
 import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
@@ -13,7 +14,30 @@ import AuthRoutes from "./auth.routes";
 function Routes(){
   const IsAuthenticated = false; //Verifica se está autenticado
   const loading = false; //Verifica se está carregando (controle de load)
-  
+
+  //Verifica se está carregando e se tiver coloca um spinner rodando
+  if(loading){
+    /**
+     * É adicionado os estilos diretamente.
+     * flex:1 (Pega a tela toda)
+     */
+    return(
+      <View 
+        style={{
+          flex:1, 
+          backgroundColor: '#1D1D2E', 
+          justifyContent: 'center', 
+          alignItems: 'center'
+        }}>
+          {/**
+           * ActivityIndicator é um spinner que fica rodando.
+           * size é o tamanho desse componete
+           */}
+          <ActivityIndicator size={60} color="#f5f7fb"/>
+      </View>
+    )
+  }
+
   return(
     //Se ele estiver true então rederiza o componente "AppRoutes" se não rederiza o "AuthRoutes"
     IsAuthenticated ? <AppRoutes/> : <AuthRoutes/>
